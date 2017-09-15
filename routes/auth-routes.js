a23688a0b322850f1e9ee5a6eb182eddda398d72
Karen Baque <k.baque@aol.com>
2017-09-15 10:22:57 -0400

    auth route set-up

---
+++ routes/auth-routes.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const authRouter = express.Router();
+const passport = require('../services/auth/local');
+const authHelpers = require('../services/auth/auth-helpers');
+const usersController = require('../controllers');
+
+authRouter.post('/register', usersController.create);
+
+authRouter.post('/login', passport.authenticate('local', {
+    successRedirect: '/auth/success',
+    failureRedirect: '/auth/failure',
+    failureFlash: true,
+}));
+
+authRouter.get('/success', (req, res) => {
+    res.json({
+       auth: true,
+       message: 'ok',
+       user: req.user, 
+    });
+});
+
+authRouter.get('/failure', (req, res) => {
+    res.json({
+        auth: false,
+        message: 'Login Failed',
+        user: null,
+    });
+});
+
+authRouter.get('/logout', (req, res) => {
+    req.logout();
+    res.json({
+        message: 'Logged Out',
+        auth: false,
+    });
+});
+
+module.exports = authRouter;
